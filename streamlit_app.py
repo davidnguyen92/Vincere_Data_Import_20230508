42f5d96adc705e5f0a804a9dc95c95bf447f3aea
Luc Nguyen <lucnguyentan92@gmail.com>
2023-05-08 16:00:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,12 +25,24 @@ import pandas as pd
 #     'second column': [10, 20, 30, 40]
 # }))
 
+# import streamlit as st
+# import numpy as np
+# import pandas as pd
+
+# chart_data = pd.DataFrame(
+#      np.random.randn(20, 3),
+#      columns=['a', 'b', 'c'])
+
+# st.line_chart(chart_data)
+
+
 import streamlit as st
 import numpy as np
 import pandas as pd
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
 
-st.line_chart(chart_data)
+    chart_data
