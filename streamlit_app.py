daa65cce6a662f5a963fee95dcb1d4927313eebd
Luc Nguyen <lucnguyentan92@gmail.com>
2023-05-08 15:56:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,16 @@ Below are our sales predictions for this customer.
 
 # df = pd.read_csv("my_data.csv")
 # st.line_chart(df)
-file = st.file_uploader("Pick a file")
+# file = st.file_uploader("Pick a file")
+
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
