d6ce2b06d5381f9e9e511ff6cfa582e4a66daf37
Luc Nguyen <lucnguyentan92@gmail.com>
2023-05-08 15:49:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,6 @@ st.write("""
 Below are our sales predictions for this customer.
 """)
 
-df = pd.read_csv("my_data.csv")
-st.line_chart(df)
+# df = pd.read_csv("my_data.csv")
+# st.line_chart(df)
+file = st.file_uploader("Pick a file")
